afc1c1e6afecc58ccbe0b14525d604941652f3d9
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-10 10:07:39 -0300

    Feat: implement basic boolean features of JavaScript in type-boolean

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+// comparando números
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
+// comparando strings
+const primeiroTexto = "Alura";
+const segundoTexto = "alura";
+
+console.log(primeiroTexto === segundoTexto);

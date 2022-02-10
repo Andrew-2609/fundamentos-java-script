346c6a1426e64fb67f9a530f8c421055e63e5d06
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-10 09:29:12 -0300

    Feat: implement unicode characters and template literal in type-string.js

--- type-string.js
+++ type-string.js
@@ -7,4 +7,9 @@ console.log("Com aspas simples: " + segundoTexto);
 const citacao = 'O professor disse "Bom dia, turma!".';
 console.log(citacao)
 
+// caracteres especiais com unicode \u
+const hiragana = '\u3041'
+
 // template string/literal
+const explicacao = `A letra "a" em japonês é escrita com o seguinte hiragana: ${hiragana}.`;
+console.log(explicacao);

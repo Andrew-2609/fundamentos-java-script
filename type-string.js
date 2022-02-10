a164f44e900f8ed7f8942dde191ffcfc4a420d99
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-10 09:22:56 -0300

    Feat: test basic string features of JavaScript in type-string.js

---
+++ type-string.js
@@ -0,0 +1,10 @@
+const primeiroTexto = "Olá, mundo!";
+const segundoTexto = 'Olá, mundo!';
+
+console.log("Com aspas duplas: " + primeiroTexto);
+console.log("Com aspas simples: " + segundoTexto);
+
+const citacao = 'O professor disse "Bom dia, turma!".';
+console.log(citacao)
+
+// template string/literal

25c7d335d381e71d346748396897d757bce0b57a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-10 09:03:45 -0300

    Feat: test basic mathematical features of JavaScript in type-number.js

---
+++ type-number.js
@@ -0,0 +1,23 @@
+// tipo Number
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacao);
+
+// NaN → Not a Number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);

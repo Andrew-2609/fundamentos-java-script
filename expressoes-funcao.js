11be002693a5f2d895980b00706083c525755999
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 08:23:53 -0300

    Feat: implement a function expression called somar() in expressoes.funcao.js

--- expressoes-funcao.js
+++ expressoes-funcao.js
@@ -1,4 +1,11 @@
- // declaração de função
- function multiplicar(primeiroNumero, segundoNumero) {
+// declaração de função
+function multiplicar(primeiroNumero, segundoNumero) {
     return primeiroNumero * segundoNumero;
- }
+}
+
+// function expression
+const soma = function (primeiroNumero, segundoNumero) {
+    return primeiroNumero + segundoNumero;
+}
+
+console.log(soma(2, 3));

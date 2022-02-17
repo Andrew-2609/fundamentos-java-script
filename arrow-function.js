aedce8fb2b09384431f7fbd26461e49c45494341
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 08:31:11 -0300

    Feat: implement two basic arrow functions in arrow-function.js

---
+++ arrow-function.js
@@ -0,0 +1,6 @@
+// Arrow Function
+const apresentar = nome => `Meu nome é ${nome}.`;
+const somar = (primeiroNumero, segundoNumero) => primeiroNumero + segundoNumero;
+
+console.log(apresentar("Andrew"));
+console.log(`2 + 3 = ${somar(2, 3)}`);

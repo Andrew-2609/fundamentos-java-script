e1f38c61ce2575509decfb6fbd7449223a5cc961
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 08:20:07 -0300

    Feat: implement a new function and invoke it inside a console.log() in parametros.js

--- parametros.js
+++ parametros.js
@@ -11,3 +11,19 @@ function multiplicar(primeiroNumero = 1, segundoNumero = 1) {
 console.log(somar(5, 5));
 console.log(multiplicar(9));
 console.log(multiplicar(somar(1, 1), somar(3, 7)));
+
+// Bônus
+
+function cumprimentar(idioma = 'pt') {
+    if (idioma === 'en') {
+        return 'Hello!';
+    } else if (idioma === 'de') {
+        return 'Hallo!';
+    } else if (idioma === 'ja') {
+        return 'こんにちは！';
+    }
+
+    return 'Olá!';
+}
+
+console.log(`${cumprimentar('ja')} Como você está?`);

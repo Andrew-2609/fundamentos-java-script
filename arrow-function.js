e660ea0c27cb38112ed35ed5e84d16c8c9d9a041
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 08:33:44 -0300

    Feat: implement arrow function with more than one line of logic in arrow-function.js

--- arrow-function.js
+++ arrow-function.js
@@ -4,3 +4,15 @@ const somar = (primeiroNumero, segundoNumero) => primeiroNumero + segundoNumero;
 
 console.log(apresentar("Andrew"));
 console.log(`2 + 3 = ${somar(2, 3)}`);
+
+// Arrow Function com mais de uma linha de instrução
+const somarNumerosMenoresQueDez = (primeiroNumero, segundoNumero) => {
+    if (primeiroNumero >= 10 || segundoNumero >= 10) {
+        return 'Operação inválida! Algum dos números passados é maior ou igual a 10.';
+    }
+
+    return primeiroNumero + segundoNumero;
+}
+
+console.log(`Tentando somar 8 com 10: ${somarNumerosMenoresQueDez(8, 10)}`);
+console.log(`Tentando somar 8 com 9: ${somarNumerosMenoresQueDez(8, 9)}`);
